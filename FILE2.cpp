57fdafea89e3ca1b610ee48c6755d782b75dd923
aludpasopati-byte <aludpasopati@gmail.com>
2026-02-24 19:58:26 +0700

    langkah 6

--- FILE2.cpp
+++ FILE2.cpp
@@ -21,3 +21,27 @@ int main() { //mulai
         cout << "terima kasih";
         //selesai
 
+
+    #include <iostream>
+using namespace std;
+
+
+int main() { //mulai
+        int p, l, luas; // deklarasi
+
+        //langkah 2
+        cout << "Masukan panjang = ";
+        cin >> p;
+
+        //langkah 3
+        cout << "Memasukan lebar =";
+        cin >> l;
+
+        //langkah 4
+        luas = p * l;
+
+        //langkah 
+        cout << "hasilnya = " << luas << endl;
+        cout << "terima kasih";
+        //selesai
+
